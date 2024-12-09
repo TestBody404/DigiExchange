7a959d2213a75d511e88fd534e13ccf0fa776138
TestBody404 <1727911759@qq.com>
2024-12-09 13:28:08 +0800

    feat(ucenter-api): initialize framework for ucenter-api

---
+++ ucenter-api/internal/handler/routes.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"github.com/zeromicro/go-zero/rest"
+	"net/http"
+)
+
+type Routers struct {
+	server      *rest.Server
+	middlewares []rest.Middleware
+}
+
+func NewRouters(server *rest.Server) *Routers {
+	return &Routers{
+		server: server,
+	}
+}
+
+func (r *Routers) Get(path string, handlerFunc http.HandlerFunc) {
+	r.server.AddRoutes(
+		rest.WithMiddlewares(
+			r.middlewares,
+			rest.Route{
+				Method:  http.MethodGet,
+				Path:    path,
+				Handler: handlerFunc,
+			},
+		),
+	)
+}
+
+func (r *Routers) Post(path string, handlerFunc http.HandlerFunc) {
+	r.server.AddRoutes(
+		rest.WithMiddlewares(
+			r.middlewares,
+			rest.Route{
+				Method:  http.MethodPost,
+				Path:    path,
+				Handler: handlerFunc,
+			},
+		),
+	)
+}
+
+func (r *Routers) Group(middlewares ...rest.Middleware) *Routers {
+	return &Routers{
+		server:      r.server,
+		middlewares: append(r.middlewares, middlewares...),
+	}
+}
